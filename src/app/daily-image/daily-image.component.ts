9d9831538cf0dbe01017b52e3a4698b8d3886be0
Ivan Sued <thefenry@gmail.com>
2018-12-11 20:20:28 -0800

    adding image quality functionality and styles

--- src/app/daily-image/daily-image.component.ts
+++ src/app/daily-image/daily-image.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { NasaDalService } from '../services/nasa-dal.service';
-import { NASAData } from '../models/nasa-data';
+import { Component, OnInit } from "@angular/core";
+import { NasaDalService } from "../services/nasa-dal.service";
+import { NASAData } from "../models/nasa-data";
 
 @Component({
-  selector: 'app-daily-image',
-  templateUrl: './daily-image.component.html',
-  styleUrls: ['./daily-image.component.less']
+  selector: "app-daily-image",
+  templateUrl: "./daily-image.component.html",
+  styleUrls: ["./daily-image.component.less"]
 })
 export class DailyImageComponent implements OnInit {
   public minimumDate: string;
   public maximumDate: string;
   public todaysDate: string;
+  public showHDImage = false;
 
   public nasaImageData: NASAData;
   public imageDate: string;
@@ -32,6 +33,10 @@ export class DailyImageComponent implements OnInit {
     this.showNASAImage(newValue.target.value);
   }
 
+  public toggleImageQuality(): void {
+    this.showHDImage = !this.showHDImage;
+  }
+
   private setTodaysDate(): void {
     this.todaysDate = this.getStringDate();
   }
@@ -47,7 +52,10 @@ export class DailyImageComponent implements OnInit {
   private showNASAImage(date: string): void {
     this.nasaDataService
       .GetNASAImage(date)
-      .subscribe((data: NASAData) => this.setNASAImageData(data), error => this.HandleRequestError(error));
+      .subscribe(
+        (data: NASAData) => this.setNASAImageData(data),
+        error => this.HandleRequestError(error)
+      );
   }
 
   private setNASAImageData(data: NASAData): void {
@@ -58,8 +66,13 @@ export class DailyImageComponent implements OnInit {
   private setImageDate(imageDate: string) {
     const currentDate = new Date(imageDate);
 
-    const monthString = currentDate.toLocaleString('en-us', { month: 'long' });
-    this.imageDate = currentDate.getFullYear() + ' ' + monthString + ' ' + currentDate.getDate();
+    const monthString = currentDate.toLocaleString("en-us", { month: "long" });
+    this.imageDate =
+      currentDate.getFullYear() +
+      " " +
+      monthString +
+      " " +
+      currentDate.getDate();
   }
 
   private getStringDate(monthDifference = 0): string {
@@ -68,15 +81,15 @@ export class DailyImageComponent implements OnInit {
     todaysDate.setMonth(todaysDate.getMonth() + monthDifference);
 
     const month = todaysDate.getMonth() + 1;
-    const monthString = month < 10 ? '0' + month : month;
+    const monthString = month < 10 ? "0" + month : month;
     const day = todaysDate.getDate();
-    const dayString = day < 10 ? '0' + day : day;
+    const dayString = day < 10 ? "0" + day : day;
 
-    return todaysDate.getFullYear() + '-' + monthString + '-' + dayString;
+    return todaysDate.getFullYear() + "-" + monthString + "-" + dayString;
   }
 
   private HandleRequestError(error: any): void {
-    alert('Something when wrong. Please refresh the page and try again.');
+    alert("Something when wrong. Please refresh the page and try again.");
     console.log(error);
   }
 }
